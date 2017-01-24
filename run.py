01507c4b6d067efbf33ea1fbeb3345e0815a234d
shiweizhi <shiwzhi@gmail.com>
2017-01-25 01:10:53 +0800

    Test log tweet

--- run.py
+++ run.py
@@ -4,32 +4,50 @@ from get_tweets import get_tweets
 import os
 from time import sleep
 
-name = "realDonaldTrump"
-
 current_dir = os.path.dirname(os.path.abspath(__file__))
 tweet_log = current_dir+"/{}.log".format(name)
 
 
-tweets = get_tweets(name)
 
-translated = []
+def get_latest():
+	name = "realDonaldTrump"
+	tweets = get_tweets(name)
+	translated = []
+	for i in tweets[:4]:
+		translated.append(translate(i, 'en', 'zh'))
 
-for i in tweets[:4]:
-	translated.append(translate(i, 'en', 'zh'))
+	# final_text = name+"\n"
 
-# final_text = name+"\n"
+	# for i in translated[:4]:
+	# 	final_text += i+"\n"
 
-# for i in translated[:4]:
-# 	final_text += i+"\n"
+	final_text = "@"+name+"\n"
 
-final_text = "@"+name+"\n"
+	for i in translated[:4]:
+		final_text += i+'\n'
 
-for i in translated[:4]:
-	final_text += i+'\n'
+	print(final_text)
+	print(len(final_text))
 
-print(final_text)
-print(len(final_text))
+	return final_text
 
-qq = qqapi.Sender("http://swz1994.com:5000")
 
-qq.send_message(614291728, final_text, "group")
+
+
+while True:
+	sleep(60)
+	if not os.path.isfile(tweet_log):
+		f = open(tweet_log, 'w')
+		f.write("init")
+		f.close()
+	latest = get_latest()
+	current = open(tweet_log, 'r').read()
+	if latest == current:
+		pass
+	else:
+		f = open(tweet_log, 'w')
+		f.write(latest)
+		f.close()
+		qq = qqapi.Sender("http://swz1994.com:5000")
+		qq.send_message(1179756234, latest, "friend")
+		
